012daf1d23494075845ee09b5f4b0739c1f5d673
barna <barna.m42@gmail.com>
2022-02-08 10:48:49 +0100

    ADD: init spring web app #1

---
+++ bankapp/src/main/java/com/codecool/bankapp/ElGrandeBancoApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElGrandeBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElGrandeBancoApplication.class, args);
+	}
+
+}
