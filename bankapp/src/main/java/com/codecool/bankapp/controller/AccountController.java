e2053322b6d87a9d9bffdd0edfe6ac8123207aaf
akos996 <balogh.akos.antal@gmail.com>
2022-02-09 15:09:51 +0100

    Add: account service class for account logic implementation

--- bankapp/src/main/java/com/codecool/bankapp/controller/AccountController.java
+++ bankapp/src/main/java/com/codecool/bankapp/controller/AccountController.java
@@ -1,8 +1,10 @@
 package com.codecool.bankapp.controller;
 
 import com.codecool.bankapp.model.Account;
-import com.codecool.bankapp.model.CheckingAccount;
 import com.codecool.bankapp.model.Transaction;
+import com.codecool.bankapp.model.User;
+import com.codecool.bankapp.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,29 +12,31 @@ import java.util.List;
 @RestController
 @RequestMapping("/account")
 public class AccountController {
+    AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @GetMapping("/history")
-    public List<Transaction> getHistory(@RequestBody Account account) {
-        return account.getHistory();
+    public List<Transaction> getHistory(@RequestParam(name = "accountNumber") String accountNumber) {
+        return accountService.getHistoryByAccount(accountNumber);
+    }
+
+    @PostMapping
+    public List<Account> getAccounts(@RequestBody User user) {
+        return accountService.getAccountsByUser(user);
     }
 
     @PostMapping("/transaction")
     public Transaction makeTransaction(@RequestBody Transaction transaction) {
-        // TODO: 2022. 02. 08. move this into the service layer
-        Account from = transaction.getAccountFrom();
-        Account to = transaction.getRecipient();
-        from.addToHistory(transaction);
-        to.addToHistory(transaction);
-        return transaction;
+        return accountService.makeTransaction(transaction);
     }
 
     @PostMapping("/ATM-deposit")
     public Transaction makeDeposit(@RequestBody Transaction depositTransaction) {
-        // TODO: 2022. 02. 08. move it into the service
-        CheckingAccount from = (CheckingAccount) depositTransaction.getAccountFrom();
-        CheckingAccount to = (CheckingAccount) depositTransaction.getRecipient();
-        from.withdrawMoney(depositTransaction.getAmount());
-        to.depositMoney(depositTransaction.getAmount());
+        accountService.makeTransaction(depositTransaction);
         return depositTransaction;
     }
 }
--- bankapp/src/main/java/com/codecool/bankapp/model/Account.java
+++ bankapp/src/main/java/com/codecool/bankapp/model/Account.java
@@ -1,18 +1,18 @@
 package com.codecool.bankapp.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Component
 public abstract class Account {
-    private final String accountNumber;
+    private String accountNumber;
     protected BigDecimal balance = new BigDecimal("0");
     @JsonBackReference
     protected final List<Transaction> history = new ArrayList<>();
@@ -40,6 +40,24 @@ public abstract class Account {
 
     public void addToHistory(Transaction transaction) {
         history.add(transaction);
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(accountNumber, account.accountNumber) && Objects.equals(balance, account.balance) && Objects.equals(history, account.history);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber, balance, history);
     }
 }
 
