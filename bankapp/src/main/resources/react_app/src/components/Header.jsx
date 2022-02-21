f783aadeb15f24ac3e8ff4d791d3efd836b4c47f
akos996 <balogh.akos.antal@gmail.com>
2022-02-21 18:59:58 +0100

    Add: timer function that will log the user out after timeout

--- bankapp/src/main/resources/react_app/src/components/Header.jsx
+++ bankapp/src/main/resources/react_app/src/components/Header.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-const Header = () => {
+const Header = (renderEvent) => {
+    const [time, setTimeLeft] = useState(300)
+
+    useEffect(() => {
+        if (time > 0) {
+            const timer = setTimeout(()=> {
+                setTimeLeft(x => x-1);
+            }, 1000);
+            return ()=> clearTimeout(timer);
+        }
+    }, [time]);
+
+    useEffect(() => {
+        setTimeLeft(300)
+    }, [renderEvent]);
+
+
+    function secondsToTime(time) {
+        let divisor_for_minutes = time % (60*60);
+        let minutes = Math.floor(divisor_for_minutes / 60);
+
+        let divisor_for_seconds = divisor_for_minutes % 60;
+        let seconds = Math.ceil(divisor_for_seconds);
+        if(seconds < 10) {
+            return `${minutes}:0${seconds}`;
+        }
+        return `${minutes}:${seconds}`;
+    }
+
+
     return (
         <ul className={"header"}>
-            <li className={"noHover"}><a>4:59</a></li>
+            <li className={"noHover"}><a>{secondsToTime(time)}</a></li>
             <li><a href="">Log out</a></li>
             <li><a href="">About</a></li>
             <li><a href="">Home</a></li>
