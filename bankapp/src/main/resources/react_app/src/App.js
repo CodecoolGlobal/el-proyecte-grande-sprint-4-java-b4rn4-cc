a30f6acbc92a74f4b0edfbbc3ee493c54e623016
barna <barna.m42@gmail.com>
2022-02-21 14:21:18 +0100

    ADD: react skeleton

---
+++ bankapp/src/main/resources/react_app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import About from "./components/About";
+
+function App() {
+  return (
+    <div className="main">
+      <div className="nav">navbar</div>
+      <div className="content">
+        <About />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ bankapp/src/main/resources/react_app/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About us</h2>
+      <div>
+        <div id="headquarters">Headquartes</div>
+      </div>
+      <div>@2022.02.21.</div>
+    </div>
+  );
+};
+
+export default About;
