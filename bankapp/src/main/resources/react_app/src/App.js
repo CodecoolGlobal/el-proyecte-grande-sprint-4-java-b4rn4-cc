346bb29a077e3e0ae95c6c31b83ac54d4031c8ab
barna <barna.m42@gmail.com>
2022-02-21 15:08:52 +0100

    ADD: basic visual navigation

--- bankapp/src/main/resources/react_app/src/App.js
+++ bankapp/src/main/resources/react_app/src/App.js
@@ -4,7 +4,24 @@ import About from "./components/About";
 function App() {
   return (
     <div className="main">
-      <div className="nav">navbar</div>
+      <div className="nav">
+        <div className="bankName">
+          <strong>El Grande Banco</strong>
+        </div>
+        <div className="navLinks">
+          <div>Account Details</div>
+          <div>
+            Services
+            <div>Deposit</div>
+            <div>Withdraw</div>
+            <div>Transaction History</div>
+            <div>Transer Money</div>
+            <div>Pay Bills</div>
+            <div>Loan</div>
+          </div>
+          <div id="about">About</div>
+        </div>
+      </div>
       <div className="content">
         <About />
       </div>
