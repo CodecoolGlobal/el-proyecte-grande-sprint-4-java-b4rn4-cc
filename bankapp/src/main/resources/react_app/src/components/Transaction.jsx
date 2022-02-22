111dc6fabf8905e4bd6d620680693bb09e9a0621
barna <barna.m42@gmail.com>
2022-02-22 11:56:02 +0100

    ADD: transaction history rendering

---
+++ bankapp/src/main/resources/react_app/src/components/Transaction.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Transaction = ({ transaction }) => {
+  return (
+    <>
+      <tbody>
+        <tr>
+          <td>{new Date(transaction.transactionTime).toDateString()}</td>
+          <td>{transaction.recipient}</td>
+          <td>{transaction.amount}</td>
+          <td
+            className={
+              transaction.status === "SUCCESSFUL"
+                ? "transferOK"
+                : "transferFail"
+            }
+          >
+            {transaction.status}
+          </td>
+        </tr>
+      </tbody>
+    </>
+  );
+};
+
+export default Transaction;
