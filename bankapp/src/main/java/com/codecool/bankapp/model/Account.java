c476dca9eb8010fac2ee0aed4bbeb2069c5bf332
akos996 <balogh.akos.antal@gmail.com>
2022-03-08 10:25:53 +0100

    Refactor: account Equals method check for account number

--- bankapp/src/main/java/com/codecool/bankapp/model/Account.java
+++ bankapp/src/main/java/com/codecool/bankapp/model/Account.java
@@ -58,7 +58,7 @@ public class Account {
         if (this == o) return true;
         if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
         Account account = (Account) o;
-        return id != null && Objects.equals(id, account.id);
+        return accountNumber != null && Objects.equals(accountNumber, account.accountNumber);
     }
 
     @Override
