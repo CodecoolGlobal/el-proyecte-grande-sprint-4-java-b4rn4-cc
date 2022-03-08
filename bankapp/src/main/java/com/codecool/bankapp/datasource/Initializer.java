1b7a178eb2334e3bb47c2c5627002afcf815d314
akos996 <balogh.akos.antal@gmail.com>
2022-03-08 13:31:18 +0100

    Feat: reimplemented Initializer to work with db

--- bankapp/src/main/java/com/codecool/bankapp/datasource/Initializer.java
+++ bankapp/src/main/java/com/codecool/bankapp/datasource/Initializer.java
@@ -31,36 +31,39 @@ public class Initializer {
                 .userID(UUID.fromString("99999999-9999-9999-9999-999999999999"))
                 .address("1234 Budapest, Hungary, Hal Street 99.").build();
         Account bankFundAccount = CheckingAccount.builder().accountNumber(UUID.fromString("00000000-0000-0000-0000-000000000000")).balance(new BigDecimal(900000000)).build();
-        accountRepository.save(bankFundAccount);
-        bank.addAccountToList(bankFundAccount);
-        userRepository.save(bank);
+        initAccount(bank, bankFundAccount);
 
+        BigDecimal money = new BigDecimal(100000);
 
-//        User user1 = User.builder().name("Bandi").password("12")
-//                .userID(UUID.fromString("11111111-2222-3333-4444-555555555555"))
-//                .address("City of Westminster, Downing Street 10.").build();
-//        User user2 = User.builder().name("Adam").password("123")
-//                .address("1111 Budapest, Csirke utca 7.")
-//                .userID(UUID.fromString("99999999-2222-3333-4444-555555555555")).build();
-//
-//        BigDecimal money = new BigDecimal(100000);
-//        userService.addUser(user1);
-//        userService.addUser(user2);
-//        user1.getAccountList().get(0).setBalance(money);
-//        user2.getAccountList().get(0).setBalance(money);
-//        UUID account1 = UUID.fromString("cd601a0a-ff98-47fc-90a9-d5fe1b5fa26c");
-//        UUID account2 = UUID.fromString("ba1ca0c3-3090-4daf-b684-0fb9a0564c71");
-//        UUID account3 = UUID.fromString("00000000-2222-1111-0000-444444444444");
-//        user1.getAccountList().get(0).setAccountNumber(account1);
-//        user2.getAccountList().get(0).setAccountNumber(account2);
-//        user2.getAccountList().get(0).setCurrency(CurrencyType.HUF);
-//        CheckingAccount secondAccount = CheckingAccount.builder().userID(user1.getUserID()).accountNumber(account3).currency(CurrencyType.GBP).build();
-//        accountService.addAccount(secondAccount, "checking");
-//        secondAccount.setBalance(money);
-//        user1.addAccountToList(secondAccount);
-//        Transaction transaction1 = Transaction.builder().amount(new BigDecimal(100)).sender(user1.getAccountList().get(0)).currency(CurrencyType.EUR).recipient(user2.getAccountList().get(0)).build();
-//        Transaction transaction2 = Transaction.builder().amount(new BigDecimal(9999999)).sender(user1.getAccountList().get(0)).currency(CurrencyType.EUR).recipient(user2.getAccountList().get(0)).build();
-//        accountService.makeTransaction(transaction1);
-//        accountService.makeTransaction(transaction2);
+        UUID user1ID = UUID.fromString("11111111-2222-3333-4444-555555555555");
+        User user1 = User.builder().name("Bandi").password("12")
+                .userID(user1ID)
+                .address("City of Westminster, Downing Street 10.").build();
+        Account account1 = CheckingAccount.builder().balance(money).currency(CurrencyType.EUR).userID(user1ID).build();
+        Account account2 = CheckingAccount.builder().currency(CurrencyType.GBP).userID(user1ID).build();
+        accountRepository.save(account1);
+        user1.addAccountToList(account1);
+        initAccount(user1, account2);
+
+        UUID user2ID = UUID.fromString("99999999-2222-3333-4444-555555555555");
+        User user2 = User.builder().name("Adam").password("123")
+                .address("1111 Budapest, Csirke utca 7.")
+                .userID(user2ID).build();
+        Account account3 = CheckingAccount.builder().balance(money).currency(CurrencyType.HUF).userID(user2ID).build();
+        initAccount(user2, account3);
+
+        makeTransaction(new BigDecimal(100), account1, account3);
+        makeTransaction(new BigDecimal(9999999), account1, account3);
+    }
+
+    private void initAccount(User user, Account account) {
+        accountRepository.save(account);
+        user.addAccountToList(account);
+        userRepository.save(user);
+    }
+
+    private void makeTransaction(BigDecimal amount, Account sender, Account recipient) {
+        Transaction transaction = Transaction.builder().amount(amount).sender(sender).currency(sender.getCurrency()).recipient(recipient).build();
+        accountService.makeTransaction(transaction);
     }
 }
