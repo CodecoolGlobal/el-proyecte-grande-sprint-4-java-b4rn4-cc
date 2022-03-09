7d38daab05ecb0540e135d116c90288d1ecae252
akos996 <balogh.akos.antal@gmail.com>
2022-03-09 22:55:37 +0100

    Fix: missing useEffect import

--- bankapp/src/main/resources/react_app/src/components/Deposit.jsx
+++ bankapp/src/main/resources/react_app/src/components/Deposit.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { useLocation } from 'react-router-dom';
 import {apiPost} from "../FetchApis";
 import {loadProp} from "./ReloadMemory";
