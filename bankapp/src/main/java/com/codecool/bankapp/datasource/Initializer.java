34dd2dd49035d37fb768ef33848b7cd1c56d2f80
akos996 <balogh.akos.antal@gmail.com>
2022-03-10 18:07:47 +0100

    Add: bill initialized in Initializer

--- bankapp/src/main/java/com/codecool/bankapp/datasource/Initializer.java
+++ bankapp/src/main/java/com/codecool/bankapp/datasource/Initializer.java
@@ -2,6 +2,8 @@ package com.codecool.bankapp.datasource;
 
 import com.codecool.bankapp.model.*;
 import com.codecool.bankapp.repository.AccountRepository;
+import com.codecool.bankapp.repository.BillRepository;
+import com.codecool.bankapp.repository.TransactionRepository;
 import com.codecool.bankapp.repository.UserRepository;
 import com.codecool.bankapp.services.AccountService;
 import com.codecool.bankapp.services.UserService;
@@ -17,13 +19,17 @@ public class Initializer {
     AccountService accountService;
     UserRepository userRepository;
     AccountRepository accountRepository;
+    TransactionRepository transactionRepository;
+    BillRepository billRepository;
 
     @Autowired
-    public Initializer(UserService userService, AccountService accountService, UserRepository userRepository, AccountRepository accountRepository) {
+    public Initializer(UserService userService, AccountService accountService, UserRepository userRepository, AccountRepository accountRepository, TransactionRepository transactionRepository, BillRepository billRepository) {
         this.userService = userService;
         this.accountService = accountService;
         this.userRepository = userRepository;
         this.accountRepository = accountRepository;
+        this.transactionRepository = transactionRepository;
+        this.billRepository = billRepository;
         Configuration.setupApp();
         init();
     }
@@ -34,6 +40,11 @@ public class Initializer {
                 .address("1234 Budapest, Hungary, Hal Street 99.").build();
         Account bankFundAccount = CheckingAccount.builder().accountNumber(UUID.fromString("00000000-0000-0000-0000-000000000000")).balance(new BigDecimal(900000000)).build();
         initAccount(bank, bankFundAccount);
+
+        User elmu = User.builder().name("ELMŰ-ÉMÁSZ Energiaszolgáltató ZRT.").password("123")
+                .address("1132 Budapest, Váci út 72-74.").build();
+        Account elmuAccount = CheckingAccount.builder().balance(new BigDecimal(10000)).build();
+        initAccount(elmu, elmuAccount);
 
         BigDecimal money = new BigDecimal(100000);
 
@@ -56,6 +67,7 @@ public class Initializer {
 
         makeTransaction(new BigDecimal(100), account1, account3);
         makeTransaction(new BigDecimal(9999999), account1, account3);
+        createBill(new BigDecimal(532), user1, elmu);
     }
 
     private void initAccount(User user, Account account) {
@@ -67,5 +79,14 @@ public class Initializer {
     private void makeTransaction(BigDecimal amount, Account sender, Account recipient) {
         Transaction transaction = Transaction.builder().amount(amount).sender((CheckingAccount) sender).currency(sender.getCurrency()).recipient(recipient).build();
         accountService.makeTransaction(transaction);
+    }
+
+    private void createBill(BigDecimal amount, User payer, User recipient) {
+        Bill bill = Bill.builder().payer(payer).recipient(recipient).build();
+        Account recipientAccount = recipient.getAccountList().get(0);
+        Transaction transaction = Transaction.builder().amount(amount).currency(recipientAccount.getCurrency()).recipient(recipientAccount).build();
+        transactionRepository.save(transaction);
+        bill.setTransaction(transaction);
+        billRepository.save(bill);
     }
 }
