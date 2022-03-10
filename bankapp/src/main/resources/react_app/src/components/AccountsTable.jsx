57ee78bc981c1a52957e7b18dcb365af50015ea6
barna <barna.m42@gmail.com>
2022-03-10 18:33:00 +0100

    ADD: styling for new pages to match the others

--- bankapp/src/main/resources/react_app/src/components/AccountsTable.jsx
+++ bankapp/src/main/resources/react_app/src/components/AccountsTable.jsx
@@ -1,33 +1,76 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
-const AccountsTable = ({accounts}) => {
-
-    return (
-        <table className="historyTable">
-            <thead>
-            <tr>
-                <td>Account Number</td>
-                <td>Account Type</td>
-                <td>Balance</td>
-                <td>Currency</td>
+const AccountsTable = ({ accounts }) => {
+  return (
+    <table className="historyTable">
+      <thead>
+        <tr>
+          <td>Account Number</td>
+          <td>Account Type</td>
+          <td>Balance</td>
+          <td>Currency</td>
+        </tr>
+      </thead>
+      <tbody>
+        {accounts &&
+          accounts.length > 0 &&
+          accounts.map((acc) => (
+            <tr key={acc.accountNumber}>
+              <td>{acc.accountNumber}</td>
+              <td>{acc.type}</td>
+              <td>{acc.balance}</td>
+              <td>{acc.currency}</td>
+              <td>
+                <button
+                  style={{
+                    backgroundColor: "#b4c7dc",
+                    height: "40px",
+                    marginBottom: "5px",
+                    borderRadius: "10px",
+                  }}
+                >
+                  <Link
+                    style={{
+                      textDecoration: "none",
+                      color: "black",
+                      fontSize: "18px",
+                    }}
+                    to="/deposit"
+                    state={{ accountNumber: acc.accountNumber }}
+                  >
+                    Deposit
+                  </Link>
+                </button>
+                <button
+                  style={{
+                    backgroundColor: "#b4c7dc",
+                    height: "40px",
+                    marginBottom: "5px",
+                    borderRadius: "10px",
+                  }}
+                >
+                  <Link
+                    style={{
+                      textDecoration: "none",
+                      color: "black",
+                      fontSize: "18px",
+                    }}
+                    to="/withdraw"
+                    state={{
+                      accountNumber: acc.accountNumber,
+                      currency: acc.currency,
+                    }}
+                  >
+                    Withdraw
+                  </Link>
+                </button>
+              </td>
             </tr>
-            </thead>
-            <tbody>
-            {accounts && accounts.length > 0 && accounts.map((acc) => (
-                <tr key={acc.accountNumber}>
-                    <td>{acc.accountNumber}</td>
-                    <td>{acc.type}</td>
-                    <td>{acc.balance}</td>
-                    <td>{acc.currency}</td>
-                    <td><Link to="/deposit" state={{accountNumber:acc.accountNumber}}>Deposit</Link><br/>
-                        <Link to="/withdraw" state={{accountNumber:acc.accountNumber, currency:acc.currency}}>Withdraw</Link>
-                    </td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
-    );
+          ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default AccountsTable;
