138f9166fefb2466d04787ed97218ad784ddb8d7
barna <barna.m42@gmail.com>
2022-03-26 15:32:32 +0100

    FIX: login error when using wrong credentials

--- bankapp/src/main/resources/react_app/src/components/Login.jsx
+++ bankapp/src/main/resources/react_app/src/components/Login.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+
+  const handler = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    fetch("/login", {
+      method: "POST",
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData),
+    }).then((response) => {
+      if (response.status === 200) {
+        navigate("/" + username);
+      }
+      // message when wrong username password combo
+    });
+  };
+
   return (
     <>
-      <form action="/login" method="post">
+      <form onSubmit={handler} action="/login" method="post">
         <h2>Please Sign In</h2>
         <div>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" placeholder="Username" required autoFocus />
+          <input
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Username"
+            required
+            autoFocus
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
