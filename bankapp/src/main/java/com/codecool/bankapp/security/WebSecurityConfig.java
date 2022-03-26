46b838585356fe3949e465b7a21857812b0bd759
barna <barna.m42@gmail.com>
2022-03-26 15:36:35 +0100

    ADD: cors configuration

--- bankapp/src/main/java/com/codecool/bankapp/security/WebSecurityConfig.java
+++ bankapp/src/main/java/com/codecool/bankapp/security/WebSecurityConfig.java
@@ -27,6 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/account/main", "/news" ,"/login").permitAll()
