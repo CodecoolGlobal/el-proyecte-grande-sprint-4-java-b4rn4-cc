fa4538126c5d6f29ad6530b0dd9c821b42da8669
barna <barna.m42@gmail.com>
2022-03-26 15:25:43 +0100

    ADD: formatting

--- bankapp/src/main/resources/react_app/src/components/Header.jsx
+++ bankapp/src/main/resources/react_app/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {apiGet} from "../FetchApis";
 
-const Header = ({handleClick, renderEvent}) => {
+const Header = ({handleClick, renderEvent, setUserId}) => {
     const [time, setTimeLeft] = useState(120);
 
     const logout = async () => {
@@ -11,36 +11,56 @@ const Header = ({handleClick, renderEvent}) => {
 
     useEffect(() => {
         if (time > 0) {
-            const timer = setTimeout(()=> {
-                setTimeLeft(x => x-1);
+            const timer = setTimeout(() => {
+                setTimeLeft((x) => x - 1);
             }, 1000);
-            return ()=> clearTimeout(timer);
+            return () => clearTimeout(timer);
         }
     }, [time]);
 
     useEffect(() => {
-        setTimeLeft(120)
+        setTimeLeft(120);
     }, [renderEvent]);
 
-
     function secondsToTime(time) {
-        let divisor_for_minutes = time % (60*60);
+        let divisor_for_minutes = time % (60 * 60);
         let minutes = Math.floor(divisor_for_minutes / 60);
 
         let divisor_for_seconds = divisor_for_minutes % 60;
         let seconds = Math.ceil(divisor_for_seconds);
-        if(seconds < 10) {
+        if (seconds < 10) {
             return `${minutes}:0${seconds}`;
         }
         return `${minutes}:${seconds}`;
     }
 
-
     return (
         <ul className={"header"}>
-            <li className={"noHover"}><a>{secondsToTime(time)} </a></li>
-            <li><a  onClick={() => {handleClick("/")}}>Log out</a></li>
-            <li><a onClick={() => {handleClick("/")}}>Home</a></li>
+            <li className={"noHover"}>
+                <button>{secondsToTime(time)} </button>
+            </li>
+            <li>
+                <button onClick={() => handleClick("/login")}>Log in</button>
+            </li>
+            <li>
+                <button
+                    onClick={() => {
+                        logout();
+                        handleClick("/");
+                    }}
+                >
+                    Log out
+                </button>
+            </li>
+            <li>
+                <button
+                    onClick={() => {
+                        handleClick("/");
+                    }}
+                >
+                    Home
+                </button>
+            </li>
         </ul>
     );
 };
